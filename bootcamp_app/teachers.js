821a586123efb4fbd21f04df384d91d0ee47fe48
Michael <michael.dburke56@gmail.com>
2021-08-18 15:34:24 -0400

    Prevent SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const queryArgs = process.argv.slice(2);
+
 console.log("\nargs: ", queryArgs)
 const pool = new Pool({
   user: 'vagrant',
@@ -8,17 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+
+pool.query(queryString, values);
